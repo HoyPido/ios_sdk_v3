7a45bf6b9b6ff730d0aa1ff767900258accaea61
Dan-Andoni-BJSS <dan.andoni@bjss.com>
2016-08-18 17:47:23 +0100

    added attribute.plist

---
+++ MobileConnectSDK/Models/DecodedTokenModel.m
@@ -0,0 +1,13 @@
+//
+//  DecodedTokenModel.m
+//  MobileConnectSDK
+//
+//  Created by Mircea Grecu on 18/08/2016.
+//  Copyright © 2016 GSMA. All rights reserved.
+//
+
+#import "DecodedTokenModel.h"
+
+@implementation DecodedTokenModel
+
+@end
---
+++ MobileConnectSDK/Utilities/TokenValidation.swift
@@ -0,0 +1,9 @@
+//
+//  TokenValidation.swift
+//  MobileConnectSDK
+//
+//  Created by Mircea Grecu on 18/08/2016.
+//  Copyright © 2016 GSMA. All rights reserved.
+//
+
+import Foundation

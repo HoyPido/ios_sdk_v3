2cf3d3a2725e54a12da484be6bdd5212c5d5b486
Dan-Andoni-BJSS <dan.andoni@bjss.com>
2016-08-30 09:04:29 +0100

    Fixed phone number request

--- MobileConnectSDK/Utilities/Networking/RequestConstructor/DiscoveryRequestConstructor.swift
+++ MobileConnectSDK/Utilities/Networking/RequestConstructor/DiscoveryRequestConstructor.swift
@@ -43,15 +43,13 @@ class DiscoveryRequestConstructor : RequestConstructor
     
     func requestWithPhoneNumber(phoneNumber : String) -> Request
     {
-        return genericDiscoveryRequestWithParameters([kKeyPhone : phoneNumber.stringByReplacingOccurrencesOfString("+", withString: "")])
+        return genericDiscoveryRequestWithParameters([kKeyPhone : phoneNumber.stringByReplacingOccurrencesOfString("+", withString: "")], isPhoneRequest: true, additionalHeaders: ["Content-Type":"application/x-www-form-urlencoded"])
     }
     
-    private func genericDiscoveryRequestWithParameters(parameters : [String : AnyObject], additionalHeaders : [String : String]? = nil, shouldNotStartImmediately : Bool = false) -> Request
+    private func genericDiscoveryRequestWithParameters(parameters : [String : AnyObject], isPhoneRequest : Bool = false, additionalHeaders : [String : String]? = nil, shouldNotStartImmediately : Bool = false) -> Request
     {
-        let isPhoneNumberRequest : Bool = parameters.contains({$0.0 == kKeyPhone})
-        
-        let method : Alamofire.Method = isPhoneNumberRequest  ? .POST : .GET
-        let encoding : ParameterEncoding = isPhoneNumberRequest ? ParameterEncoding.URL : ParameterEncoding.URLEncodedInURL
+        let method : Alamofire.Method = isPhoneRequest  ? .POST : .GET
+        let encoding : ParameterEncoding = isPhoneRequest ? ParameterEncoding.URL : ParameterEncoding.URLEncodedInURL
         
         return requestWithMethod(method, url: applicationEndpoint, parameters: [kRedirectURL : redirectURL.URLString] + parameters, encoding: encoding, additionalHeaders: additionalHeaders, shouldNotStartImmediately : shouldNotStartImmediately)
     }

0741c03a2be0291fbf78b53198adc3bf0a91799c
Dan-Andoni-BJSS <dan.andoni@bjss.com>
2016-10-06 14:37:19 +0100

    * Fix Scope sent for nationalID

--- DemoSwift/IdentityNationalIdViewController.swift
+++ DemoSwift/IdentityNationalIdViewController.swift
@@ -45,7 +45,7 @@ class IdentityNationalIdViewController: UIViewController {
     @IBAction func getToken() {
         let manager : MobileConnectManager = MobileConnectManager()
         if isCalledDiscoveryWithPhoneNumber {
-            manager.getAttributeServiceResponseWithPhoneNumber(phoneNumberTextField.text ?? "", inPresenterController: self, withStringValueScopes: [ProductType.Address], context: "MC", bindingMessage: nil, completionHandler: launchTokenViewerWithAttributeServiceResponse)
+            manager.getAttributeServiceResponseWithPhoneNumber(phoneNumberTextField.text ?? "", inPresenterController: self, withStringValueScopes: [ProductType.IdentityNationalID], context: "MC", bindingMessage: nil, completionHandler: launchTokenViewerWithAttributeServiceResponse)
         } else {
             manager.getAttributeServiceResponse(self, context: "MC", scopes: [ProductType.IdentityNationalID], withCompletionHandler: launchTokenViewerWithAttributeServiceResponse)
         }

9eaa6be937ca788503a0ac7ecaebe57cde5926f6
dMaretsky <user@gsma0.itransition.corp>
2017-06-15 17:42:33 +0300

    deleted bridge files

--- MobileConnectSDK/Models/BridgingHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  BridgingHeader.h
-//  MobileConnectSDK
-//
-//  Created by user on 4/19/17.
-//  Copyright © 2017 GSMA. All rights reserved.
-//
-#include <ifaddrs.h>
-
--- MobileConnectSDK/Models/BridgingHeader.m
+++
@@ -1,9 +0,0 @@
-//
-//  BridgingHeader.m
-//  MobileConnectSDK
-//
-//  Created by user on 4/19/17.
-//  Copyright © 2017 GSMA. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
